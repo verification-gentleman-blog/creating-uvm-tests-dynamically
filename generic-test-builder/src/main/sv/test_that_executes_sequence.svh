f5cc0c68019d9935778a65e82cc988a0dd356e58
Tudor Timi <tudortimi@users.noreply.github.com>
2022-11-20 21:09:22 +0200

    Fix null pointer exception when simulator calls overloaded function during object construction

--- generic-test-builder/src/main/sv/test_that_executes_sequence.svh
+++ generic-test-builder/src/main/sv/test_that_executes_sequence.svh
@@ -10,6 +10,12 @@ class test_that_executes_sequence #(type T = uvm_test) extends T;
 
 
   virtual function string get_type_name();
+    // `get_type_name()` gets called through the base class' constructor, before `seq_wrapper`
+    // has been set. In some simulators, the overloaded version of `get_type_name()` (i.e. this
+    // function) gets called, not the version present in the class where it's called from.
+    if (seq_wrapper == null)
+      return super.get_type_name();
+
     return $sformatf("test_that_executes_%s", seq_wrapper.get_type_name());
   endfunction
 
